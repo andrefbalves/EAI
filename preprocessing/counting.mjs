0ab1420aaf5fbded98c444162185a2b203211910
Alves <andrefbalves>
2022-04-11 19:12:22 +0100

    Fiquei na Lab3 .3

--- preprocessing/counting.mjs
+++ preprocessing/counting.mjs
@@ -1,33 +1,29 @@
 import {ngram} from "./tokenization.mjs";
 
-function words(text) {
+export function words(text) {
     return text.split(' ').length;
 }
 
-function characters(text) {
+export function characters(text) {
     return text.replace(/\s/gm,'').length;
+}
+
+export function exists(term, text) {
+    return JSON.stringify(term) === JSON.stringify(text);
 }
 
 function numberOfOccurrences(term, text) {
     let array = [];
     let count = 0;
 
-    if (Array.isArray(term)) {
-        array = ngram(term.length, text.split(' '));
+    array = ngram(Array.isArray(term) ? term.length : 1, text.split(' '));
 
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === term) count++;
-        }
-    }else {
-        array = ngram(1, text.split(' '));
+    for (let i = 0; i < array.length; i++) {
+        if (exists(term, array[i])) count++;
     }
 
-
-
-    //array.filter((v) => (v == term)).length
-
-    //return array.filter((v) => (v == term)).length;
     return count;
 }
 
-console.log(numberOfOccurrences(['really', 'Interesting'],'a really really really Interesting string with some words'));
+console.log(numberOfOccurrences(['really', 'really'], 'a really really really Interesting string with some words'));
+console.log(numberOfOccurrences( ['really'],'a really really really Interesting string with some words'));
